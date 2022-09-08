390a76558435ad8d1f9aca51e4c781bea97e468c
KevinNDoan <83600888+KevinNDoan@users.noreply.github.com>
2022-09-08 13:10:21 -0700

    Updates

--- pages/about.js
+++ pages/about.js
@@ -5,6 +5,13 @@ export default function About() {
     return(
         <>
             <Header pageName={'About'} />
+
+            <main>
+                <div className="d-flex w-50 justify-center">
+                    <h1 className="text-lg">Dedicated 12,000 sq. ft. facility in Irvine, CA housing automated SMT lines and complete product manufacturing</h1>
+                </div>
+            </main>
+
             <Footer />
         </>
     )
