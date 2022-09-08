e6b7d7411c128b5c9303be34c91276bbf03f97b2
KevinNDoan <83600888+KevinNDoan@users.noreply.github.com>
2022-09-08 10:20:41 -0700

    Updates

---
+++ components/header.js
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import styles from '../styles/Navbar.module.css'
+
+export default function Header() {
+    return(
+        <>
+        <Head>
+            <title>Stracon {props.pageName}</title>
+            <meta name="description" content="Full Service Contract Manufacturing" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Navbar pageName={props.pageName} />
+        </>
+    )
+}
+
+function Navbar() {
+    return(
+        <div className={styles.navbar}>
+            <section className={styles.logo}></section>
+            <section className={styles.menu}>
+                <Link href="/home">Home</Link>
+                <Link href="/about">About</Link>
+                <Link href="/equipment">Equipment</Link>
+                <Link href="/industries">Industries</Link>
+                <Link href="/technologies">Technologies</Link>
+                <Link href="/value-added">Value Added</Link>
+                <Link href="/contact">Contact Us</Link>
+            </section>
+        </div>
+    )
+}
