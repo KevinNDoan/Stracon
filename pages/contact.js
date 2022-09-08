15d50f3c8f330ac2dd52993d4fd95a3d3a332143
KevinNDoan <83600888+KevinNDoan@users.noreply.github.com>
2022-09-08 13:06:29 -0700

    Updates

--- pages/contact.js
+++ pages/contact.js
@@ -4,6 +4,11 @@ export default function Contact() {
     return(
         <>
             <Header pageName={'Contact'} />
+            
+            <main>
+                <h1 className="text-lg">Dedicated 12,000 sq. ft. facility in Irvine, CA housing automated SMT lines and complete product manufacturing</h1>
+            </main>
+
             <Footer />
         </>
     )
