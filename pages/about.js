aa9f0e53b13f99879f2e6a5cd6f7f22d9ece8ca3
KevinNDoan <83600888+KevinNDoan@users.noreply.github.com>
2022-09-08 13:41:21 -0700

    Updating about

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Header from '../components/header.js'
 import Footer from '../components/footer.js'
 
@@ -8,9 +9,12 @@ export default function About() {
 
             <main>
                 <div className="flex justify-center mt-10">
-                    <h1 className="text-xl w-1/2">Our dedicated 12,000 sq. ft. facility in Irvine, California houses automated surface mount technology lines and complete product manufacturing.</h1>
-                    <br />
-                    <h1 className="text-xl w-1/2">Surface mount assembly</h1>
+                    <div>
+                        <h1 className="text-xl w-1/2">Our dedicated 12,000 sq. ft. facility in Irvine, California houses automated surface mount technology lines and complete product manufacturing.</h1>
+                        <br />
+                        <Image src="https://straconinc.com/images/slider/slide1.jpg" />
+                        <h1 className="text-xl w-1/2">Surface mount assembly</h1>
+                    </div>
                 </div>
 
                 {/** Photo checklist
