0fd68f90a090273c9e7f5296167a1fe02f05ac33
KevinNDoan <83600888+KevinNDoan@users.noreply.github.com>
2022-09-08 13:26:13 -0700

    Updating about

--- pages/about.js
+++ pages/about.js
@@ -7,8 +7,8 @@ export default function About() {
             <Header pageName={'About'} />
 
             <main>
-                <div className="flex justify-center">
-                    <h1 className="text-lg w-1/2">Dedicated 12,000 sq. ft. facility in Irvine, CA housing automated SMT lines and complete product manufacturing</h1>
+                <div className="flex justify-center mt-10">
+                    <h1 className="text-xl w-1/2">Our dedicated 12,000 sq. ft. facility in Irvine, California houses automated surface mount technology lines and complete product manufacturing.</h1>
                 </div>
             </main>
 
