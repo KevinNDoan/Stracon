036c94f2bbde5fcf192e3fc7062293a63aed0ee7
KevinNDoan <83600888+KevinNDoan@users.noreply.github.com>
2022-09-08 11:49:50 -0700

    Updates

--- components/header.js
+++ components/header.js
@@ -50,25 +50,27 @@ class Navbar extends React.Component {
     
     render() {
         return(
-            <div className={`flex bg-white`}>
+            <div className={`flex bg-white justify-between`}>
                 <section className={styles.logo}></section>
-                <section className={`${styles.menu}`}>
-                    <Link href="/">Home</Link>
-                    <Link href="/about">About</Link>
-                    <Link href="/equipment">Equipment</Link>
-                    <Link href="/industries">Industries</Link>
-                    <Link href="/technologies">Technologies</Link>
-                    <Link href="/value-added">Value Added</Link>
-                    <Link href="/contact">Contact Us</Link>
-                </section>
-                <section className={`${styles.smallMenu}`}>
-                    <div onClick={this.toggleMenu}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                        </svg>
+                <section>
+                    <div className={`${styles.menu}`}>
+                        <Link href="/">Home</Link>
+                        <Link href="/about">About</Link>
+                        <Link href="/equipment">Equipment</Link>
+                        <Link href="/industries">Industries</Link>
+                        <Link href="/technologies">Technologies</Link>
+                        <Link href="/value-added">Value Added</Link>
+                        <Link href="/contact">Contact Us</Link>
                     </div>
-                    <div className={`${this.state.hiddenMenu}`}>
-                        
+                    <div className={`${styles.smallMenu}`}>
+                        <div className="clickable" onClick={this.toggleMenu}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                            </svg>
+                        </div>
+                        <div className={`${this.state.hiddenMenu}`}>
+                            
+                        </div>
                     </div>
                 </section>
             </div>
