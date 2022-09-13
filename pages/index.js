91133f2d8b0659aec7fe18ea5d0bd69767b8f596
Kevin <kevindoan2004@gmail.com>
2022-09-13 07:57:18 -0700

    Updating basic information

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
           <section className="bg-black w-100 p-20">
             <div className="flex justify-center">
               <div>
-                <h1 className="text-white text-4xl md:text-6xl font-bold">ISO 9001:2015 and</h1>
+                <h1 className="text-white text-4xl md:text-6xl font-bold">AS9100D and</h1>
                 <h1 className="text-white text-4xl md:text-6xl font-bold">ISO 13485:2016 Certified</h1>
                 <br />
                 <h1 className="text-white md:text-2xl">...and much more. We have the know-how you need to get what you want done.</h1>
@@ -32,33 +32,39 @@ export default function Home() {
           </section>
           <br />
           <section className="p-5 md:p-20">
-            <h1 className="text-4xl md:text-6xl font-bold">Get to know Stracon</h1>
+            <div className="text-center md:text-left"><h1 className="text-4xl md:text-6xl font-bold">Get to know Stracon</h1></div>
             <br />
             <div className="w-full flex justify-center">
               <div className="w-full grid grid-cols-1 md:grid-cols-3">
-                <Link href="/about">
-                  <div className={`${styles.card} clickable`}>
-                    <h2 className="text-3xl font-bold">About &rarr;</h2>
-                    <br />
-                    <p>Discover how Stracon can give you an advantage in today&apos;s competitive market.</p>
-                  </div>
-                </Link>
+                <div className="flex justify-center">
+                  <Link href="/about">
+                    <div className={`${styles.card} clickable`}>
+                      <h2 className="text-3xl font-bold">About &rarr;</h2>
+                      <br />
+                      <p>Discover how Stracon can give you an advantage in today&apos;s competitive market.</p>
+                    </div>
+                  </Link>
+                </div>
 
-                <Link href="/contact">
-                  <div className={`${styles.card} clickable`}>
-                    <h2 className="text-3xl font-bold">Contact us &rarr;</h2>
-                    <br />
-                    <p>Reach out to us by email, phone, or fax.</p>
-                  </div>
-                </Link>
+                <div className="flex justify-center">
+                  <Link href="/contact">
+                    <div className={`${styles.card} clickable`}>
+                      <h2 className="text-3xl font-bold">Contact us &rarr;</h2>
+                      <br />
+                      <p>Reach out to us by email, phone, or fax.</p>
+                    </div>
+                  </Link>
+                </div>
 
-                <Link href="/industries">
-                  <div className={`${styles.card} clickable`}>
-                    <h2 className="text-3xl font-bold">Industries &rarr;</h2>
-                    <br />
-                    <p>Find out the many industries Stracon serves everyday.</p>
-                  </div>
-                </Link>
+                <div className="flex justify-center">
+                  <Link href="/industries">
+                    <div className={`${styles.card} clickable`}>
+                      <h2 className="text-3xl font-bold">Industries &rarr;</h2>
+                      <br />
+                      <p>Find out the many industries Stracon serves everyday.</p>
+                    </div>
+                  </Link>
+                </div>
               </div>
             </div>
           </section>
